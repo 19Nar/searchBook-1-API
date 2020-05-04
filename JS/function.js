5ef273052c9d4449c2435c87f2bc09a96e21ed8a
19Nar <60644251+19Nar@users.noreply.github.com>
2020-05-04 18:11:58 +0200

    Update function.js

--- JS/function.js
+++ JS/function.js
@@ -18,24 +18,24 @@ const showBookResults = (allResults) => {
 
     html += `
 
+  <hr>
   
-    <hr>
-         <div class="container">
-  <div class="row">
-    <div class="col-sm-2 p-2 text-secondary text-center">
-      <span id="text-center">Language: ${book.volumeInfo.language}</span>
-    </div>
-    <div class="col-sm-2 p-2 text-center text-secondary">
-      <span id="text-center">Title: ${book.volumeInfo.title}</span>
-    </div>
-    <div class="col-sm-4 p-2 text-center text-secondary">
-      <span id="text-center">Category: ${book.volumeInfo.categories} </span>
-    </div>
-    <div class="col-sm-4 p-2 text-center text-secondary">
-      <span id="text-center">Published Date: ${book.volumeInfo.publishedDate}</span>
-    </div>
+       <div class="container">
+<div class="row">
+  <div class="col-sm-2 p-2 text-secondary text-center">
+    <span id="text-center">Language: ${book.volumeInfo.language}</span>
   </div>
+  <div class="col-sm-2 p-2 text-center text-secondary">
+    <span id="text-center">Title: ${book.volumeInfo.title}</span>
   </div>
+  <div class="col-sm-4 p-2 text-center text-secondary">
+    <span id="text-center">Category: ${book.volumeInfo.categories} </span>
+  </div>
+  <div class="col-sm-4 p-2 text-center text-secondary">
+    <span id="text-center">Published Date: ${book.volumeInfo.publishedDate}</span>
+  </div>
+</div>
+</div>
        `
   })
   results.innerHTML = html;
